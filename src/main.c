fd11fea7363ab63b9d9a5d089131962f861bc6ca
RandmC13 <75956767+RandmC13@users.noreply.github.com>
2024-06-18 14:03:34 +0100

    Implemented basic game flow, it is BADLY implemented and has many issues, fix asap

--- src/main.c
+++ src/main.c
@@ -68,7 +68,14 @@ int main(int argc, char **argv)
     Hand *hand = generateHand(startingCards[0], startingCards[2]);
     //Create dealer's hand from deck
     Hand *dealerHand = generateHand(startingCards[1], startingCards[3]);
-    char dealerTurn = 0;
+    //Define booleans for game logic
+    char gameTurn = 0; //0 = player's turn, 1 = dealer's turn, 2 = game over
+    char gameOutcome = 0; //0 = player wins, 1 = dealer wins. 2 = even
+    char playerBust = 0;
+    char dealerBust = 0;
+
+    //If player's starting hand is blackjack then cut straight to dealer's turn
+    if (hand->total == 21) gameTurn = 1;
 
     //Define variables for drawing hands and the deck
     float deckPad = 10.0f;
@@ -89,14 +96,78 @@ int main(int argc, char **argv)
         //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
         u32 kDown = hidKeysDown();
 
+        // TODO: Add animations for drawing cards and such
+        // - Animation for turning over dealer's face down card?
+        //
+        // TODO: Add extra rules like splitting, double down, insurance, etc.
+        //
         if (kDown & KEY_START) break;
-        if (kDown & KEY_A && hand->size <= 12) addCardToHand(hand, dealCard(deck));
-        if (kDown & KEY_B && dealerHand->size <= 12) {
-            dealerTurn = !dealerTurn;
+
+        switch (gameTurn) {
+            case 0: //Player's turn
+                if (kDown & KEY_A) { //Hit
+                    addCardToHand(hand, dealCard(deck));
+                    //Player can only go bust after hitting so only need to check here
+                    if (hand->total > 21) {playerBust = 1; gameTurn = 2; break;}
+                    if (hand->total == 21) {gameTurn = 1; break;}
+                }
+                //If control flow passes this point the player is neither bust nor blackjack so they can choose to stand
+                if (kDown & KEY_B) gameTurn = 1; //Stand
+                break;
+            case 1: //Dealer's turn
+                //Dealer must hit until they go over 16
+                while (dealerHand->total < 17) {
+                    addCardToHand(dealerHand, dealCard(deck));
+                }
+                if (dealerHand->total > 21) {dealerBust = 1; gameTurn = 2; break;}
+                //If control flow gets to here dealer must stand
+                gameTurn = 2;
+                break;
+            case 2:
+                switch (playerBust) {
+                    case 0:
+                        if (dealerBust) {gameOutcome = 0; break;}
+                        if (hand->total > dealerHand->total) {gameOutcome = 0; break;}
+                        if (hand->total == dealerHand->total) {gameOutcome = 2; break;}
+                        gameOutcome = 1;
+                        break;
+                    case 1:
+                        if (dealerBust) {gameOutcome = 2; break;}
+                        gameOutcome = 1;
+                        break;
+                }
         }
 
-        printf("\x1b[15;6HDealer's score Is: %d", dealerHand->total);
-        printf("\x1b[16;6HPlayer's score Is: %d", hand->total);
+        // NOTE: this is temporary
+        switch (gameTurn) {
+            case 0:
+                printf("\x1b[15;6HPlayer's Turn");
+                printf("\x1b[16;6HPlayer has: %d", hand->total);
+                break;
+            case 1:
+                printf("\x1b[15;6HDealer's Turn");
+                printf("\x1b[16;6HPlayer has: %d", hand->total);
+                printf("\x1b[17;6HDealer has: %d", dealerHand->total);
+                break;
+            case 2:
+                switch (gameOutcome) {
+                    case 0:
+                        printf("\x1b[16;6HPlayer has: %d", hand->total);
+                        printf("\x1b[17;6HDealer has: %d", dealerHand->total);
+                        printf("\x1b[20;6HPlayer Wins!");
+                        break;
+                    case 1:
+                        printf("\x1b[16;6HPlayer has: %d", hand->total);
+                        printf("\x1b[17;6HDealer has: %d", dealerHand->total);
+                        printf("\x1b[20;6HDealer Wins");
+                        break;
+                    case 2:
+                        printf("\x1b[16;6HPlayer has: %d", hand->total);
+                        printf("\x1b[17;6HDealer has: %d", dealerHand->total);
+                        printf("\x1b[20;6HTie");
+                        break;
+                }
+        }
         //Begin a frame
         C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
         //Clear screens
@@ -105,16 +176,19 @@ int main(int argc, char **argv)
 
         //Draw top screen
         C2D_SceneBegin(top);
-        //Draw table assets
+        //Draw table markings
         C2D_DrawSprite(&assets);
 
         //Draw cards
         drawDeckPile(deck, &back, deckOffset, deckPad, deckPad);
         drawHand(hand, &cardsheet, handPadding);
-        drawDealerHand(dealerHand, &cardsheet, &back, dealerTurn, dealerX, dealerY); //Draw dealer's hand in line with the deck for aestheticness
+        drawDealerHand(dealerHand, &cardsheet, &back, gameTurn, dealerX, dealerY); //Draw dealer's hand in line with the deck for aestheticness
 
         //Draw bottom screen
         // C2D_SceneBegin(bottom);
+        // TODO: Add chip pile at bottom screen that changes in size with wins/losses (have accurate chip colour denominations)
+        // TODO: Add betting area where chips are moved when a bet is placed
+
         //End frame
         C3D_FrameEnd(0);
     }
